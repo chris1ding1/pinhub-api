ab88515d1099cc7a1cdab7c4ed519512589cb9ba
chris <chris1ding1@gmail.com>
2025-06-24 00:59:08 +0800

    get setting, auth.

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.config import Settings
-from app.routers import pins, users
+from app.routers import auth
 
 app = FastAPI()
 
@@ -16,16 +16,15 @@ def get_settings():
     """Creating the Settings only once"""
     return Settings()
 
-
 # Set all CORS enabled origins
-if Settings.all_cors_origins:
+if get_settings().all_cors_origins:
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=Settings.all_cors_origins,
+        allow_origins=get_settings().all_cors_origins,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
     )
-
-app.include_router(pins.router)
-app.include_router(users.router)
+#app.include_router(pins.router)
+#app.include_router(users.router)
+app.include_router(auth.router)
