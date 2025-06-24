3571c4ef627f5dcbc683bc4f217c068de7d3f52c
chris <chris1ding1@gmail.com>
2025-06-25 02:53:29 +0800

    Dev auth_email_verify func

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -8,10 +8,13 @@ router = APIRouter()
 class AuthEmail(BaseModel):
     email: EmailStr = Field(max_length=255)
 
+class AuthEmailVerify(AuthEmail):
+    verifyCode: str = Field(min_length=6, max_length=6)
+
 @router.post("/auth/email/send")
-async def auth_redirect(authEmail: AuthEmail):
+async def auth_email_send(authEmail: AuthEmail):
     return await send_email(authEmail.email, "Please verify your email address")
 
 @router.post("/auth/email/verify")
-async def auth_callback():
-    return "xxx"
+async def auth_email_verify(authEmailVerify: AuthEmailVerify):
+    return {"email": authEmailVerify.email, "verifyCode": authEmailVerify.verifyCode}
