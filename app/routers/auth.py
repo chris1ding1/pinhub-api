0d86c47562bef2969bfe2a57799f6e96db1e4bd9
chris <chris1ding1@gmail.com>
2025-06-24 15:14:57 +0800

    Dev. Call send email verify.

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter
 from pydantic import BaseModel, EmailStr, Field
+from app.utils import send_email, generate_random_string
+from app.config import Settings
+
+settings = Settings()
 
 router = APIRouter()
 
@@ -8,7 +12,17 @@ class AuthEmail(BaseModel):
 
 @router.post("/auth/email/send")
 async def auth_redirect(authEmail: AuthEmail):
-    return authEmail
+
+    verification_code = generate_random_string()
+    email_html_content = f"""
+    <html>
+        <body>
+            <p>Please use this code to verify your {settings.APP_NAME} email address:</p>
+            <h2>{verification_code}</h2>
+        </body>
+    </html>
+    """
+    return await send_email(authEmail.email, f"Please verify your email address for {settings.APP_NAME}", email_html_content)
 
 @router.post("/auth/email/verify")
 async def auth_callback():
