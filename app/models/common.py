9d74a491f315b341d3eea1e84f72ed8f00371f62
chris <chris1ding1@gmail.com>
2025-06-26 20:17:17 +0800

    Define Index resource model.

--- app/models/common.py
+++ app/models/common.py
@@ -1,6 +1,9 @@
-from typing import Any
+from typing import Any, List
 from enum import IntEnum
 from pydantic import BaseModel
+from app.config import Settings
+
+settings = Settings()
 
 class StatusCode(IntEnum):
     SUCCESS = 0
@@ -8,3 +11,11 @@ class StatusCode(IntEnum):
 class ApiResponse(BaseModel):
     code: StatusCode = StatusCode.SUCCESS
     data: Any | None = None
+
+class IndexResource(BaseModel):
+    total: int = 0
+    page: int = 1
+    page_size: int = settings.PAGE_SIZE
+    next_page: int = 0
+    prev_page: int = 0
+    items: List[Any]
