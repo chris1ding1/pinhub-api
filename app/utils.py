59675af7d756c66f3417b534e5c14a92d7417bfb
chris <chris1ding1@gmail.com>
2025-07-05 14:15:56 +0800

    Add get_path_segment func.

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,6 @@
 import secrets
 import string
+from pathlib import Path
 
 from app.config import Settings
 
@@ -8,3 +9,13 @@ settings = Settings()
 def generate_random_string(length=6):
     charset = string.digits + string.ascii_lowercase
     return ''.join(secrets.choice(charset) for _ in range(length))
+
+def get_path_segment(path: str, index: int) -> str | None:
+    if not path:
+        return None
+
+    path_obj = Path(path)
+    parts = path_obj.parts
+    if len(parts) > index:
+        return parts[index]
+    return None
