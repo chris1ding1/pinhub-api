73772410c91c11e1fce907792dce224cf269c31c
chris <chris1ding1@gmail.com>
2025-07-05 14:16:55 +0800

    verify pins.store.

--- app/routers/pins.py
+++ app/routers/pins.py
@@ -1,8 +1,10 @@
 import filetype
 from typing import Annotated
-
+from urllib.parse import urlparse
 from fastapi import APIRouter, File, UploadFile, HTTPException
 
+from app.utils import get_path_segment
+from app.services.aws import get_aws_service
 from app.services.pins import PinsService
 from app.models.common import ApiResponse
 from app.models.pin import PinFormCreate, PinPublic
@@ -16,6 +18,23 @@ settings = Settings()
 
 @router.post("/pins")
 async def store(pinFormCreate: PinFormCreate, user: CurrentUser, session: SessionDep, response_model=ApiResponse):
+    if (not pinFormCreate.content or pinFormCreate.content.strip() == "") and (not pinFormCreate.url or pinFormCreate.url.strip() == "") and (not pinFormCreate.image_path or pinFormCreate.image_path.strip() == ""):
+        raise HTTPException(status_code=422)
+
+    if pinFormCreate.url:
+        url_obj = urlparse(pinFormCreate.url)
+        if not url_obj.netloc:
+            raise HTTPException(status_code=422)
+
+    if pinFormCreate.image_path:
+        image_path_user_id = get_path_segment(pinFormCreate.image_path, 1)
+        if image_path_user_id != str(user.id):
+            raise HTTPException(status_code=400)
+        aws_service = get_aws_service()
+        image_path_meta = aws_service.get_s3_head_object(settings.ASSET_STORAGE_BUCKET_NAME, pinFormCreate.image_path)
+        if not image_path_meta:
+           raise HTTPException(status_code=400)
+
     pins_service = PinsService(session)
     pin = pins_service.store(pinFormCreate, user)
     session.refresh(pin)
@@ -39,7 +58,7 @@ async def file_store(
     upLoadResult = PinsService.uplpad_file(file_content, user)
     if upLoadResult is False:
         raise HTTPException(status_code=500)
-    return ApiResponse(data={"name":upLoadResult["name"]})
+    return ApiResponse(data=upLoadResult)
 
 @router.put("/pins/{id}")
 async def update(id: str, user: CurrentUser):
